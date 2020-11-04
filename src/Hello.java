4ed084cd2795f28bdf4b04d8935a1027e2fad88f
prcedp <prcedp@192.168.100.102>
2020-11-04 14:28:26 +0800

    test1

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 
 public class Hello {
 	 public static void main(String[] args) {
-	        System.out.println("Hello Shen Zhen he guang zhou  ");
+	        System.out.println("Hello Shen Zhen he guang zhou ! ");
 	    }
 
 }
